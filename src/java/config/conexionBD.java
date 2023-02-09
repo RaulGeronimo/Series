7e07ede0b9e1856dad0dd9d78498d182cc2c4e49
Raul Geronimo <raul090700@gmai.com>
2023-02-08 18:54:00 -0600

    Final

--- src/java/config/conexionBD.java
+++ src/java/config/conexionBD.java
@@ -16,7 +16,7 @@ public class conexionBD {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         
-        dataSource.setUrl("jdbc:mysql://localhost:3306/Series");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/Series?sessionVariables=LC_TIME_NAMES='es_MX'");
         dataSource.setUsername("root");
         dataSource.setPassword("");
         return dataSource;
